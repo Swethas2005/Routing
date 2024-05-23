3ffa5f3614713ce56a3e144bb1fe5d65426eb63e
Swethas2005 <swethas15092005@gmail.com>
2024-05-23 20:30:38 +0530

    completed

---
+++ a3p4_437863/a3p4/src/pages/Courses.jsx
@@ -0,0 +1,11 @@
+// src/pages/Courses.jsx
+
+// Courses component definition
+export default function Courses() {
+    return (
+      <>
+        <h1>Courses Page</h1> {/* Displaying Courses Page header */}
+      </>
+    );
+  }
+  
